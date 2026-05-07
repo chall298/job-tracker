c388f200f9768dbe49a7d3c27117468f929ca48b
Christopher Hall <49170970+chall298@users.noreply.github.com>
2026-05-07 18:56:33 -0400

    testing

--- middleware.ts
+++ middleware.ts
@@ -25,7 +25,7 @@ export async function middleware(request: NextRequest) {
         }
     )
 
-    // Refreshes session if expired
+    // Refreshes session if expired**
     await supabase.auth.getUser
 
     return response
